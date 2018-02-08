0e402337554798a2e009d88dc973016141b2bc46
Shreyas Tawre <stawre@cs.utexas.edu>
2018-02-08 14:53:53 -0600

    Missing 'converged' function

--- lab1.cpp
+++ lab1.cpp
@@ -3,15 +3,21 @@
 #include <unistd.h>
 #include <getopt.h>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
+int k;
+float threshold;
+int iterations;
+int workers;
+char* input;
+
 class Point {
-private:
+public:
 	vector<float> coordinates;
 	int label;
 
-public:
 	Point(vector<float>& coordinates) {
 		int size = coordinates.size();
 		for(int i = 0; i < size; i++) {
@@ -19,50 +25,41 @@ public:
 		}
 	}
 
-	float getCoordinate(int index) {
-		return coordinates[index];
-	}
-
 	float getDimensionsCount() {
 		return coordinates.size();
 	}
 };
 
-class Cluster {
-private:
+class Centroid {
+public:
 	int label;
 	vector<Point> points;
-	vector<float> location;
+	vector<float> coordinates;
 
-public:
-	Cluster(int label, Point point) {
+	Centroid(int label, Point point) {
 		this->label = label;
 		int count = point.getDimensionsCount();
 		for(int i = 0; i < count; i++) {
-			location.push_back(point.getCoordinate(i));
+			coordinates.push_back(point.coordinates[i]);
 		}
 		points.push_back(point);
 	}
-
-	Point getPoint(int index) {
-		return points[index];
-	}
 };
 
-vector<Point> randomCentroids(vector<Point> points, int k) {
-	vector<Point> retval;
+vector<Centroid> randomCentroids(vector<Point> points, int k) {
+	vector<Centroid> retval;
 
 	for(int i = 0; i < k; i++) {
 		while (1) {
 			int index = rand() % points.size();
 			if (i > 0) {
-				if (retval[i-1] == points[i]) {
+				if (retval[i-1].coordinates == points[i].coordinates) {
 					break;
 				}
 			}
 
-			retval.push_back(points[index]);
-			Cluster cluster(i, points[index]);
+			Centroid* centroid = new Centroid(i, points[index]);
+			retval.push_back(*centroid);
 			break;
 		}
 	}
@@ -71,28 +68,28 @@ vector<Point> randomCentroids(vector<Point> points, int k) {
 }
 
 
-void findNearestCentroids(vector<Point> points, vector<Point> centroids) {
+void findNearestCentroids(vector<Point> points, vector<Centroid> centroids) {
 	int total_points = points.size();
-	int total_coordinates = points.getDimensionsCount();
+	int total_coordinates = points[0].getDimensionsCount();
 
-	for (int i = 0; i < total; i++) {
+	for (int i = 0; i < total_points; i++) {
 		Point curr_point = points[i];
 
 		float sum_diff = 0;
 		float min;
 
 		for (int j = 0; j < total_coordinates; j++) {
-			sum_diff += pow(clusters[0].location[j] - curr_point.coordinates[j], 2.0);
+			sum_diff += pow(centroids[0].coordinates[j] - curr_point.coordinates[j], 2.0);
 		}
 
-		min = sqrt(sum);
+		min = sqrt(sum_diff);
 
 		for (int j = 1; j < k; j++) {
 			float dist;
 			float sum = 0;
 
 			for(int m = 0; m < total_coordinates; m++) {
-				sum += pow(clusters[j].location[m] - curr_point.coordinates[m], 2.0);
+				sum += pow(centroids[j].coordinates[m] - curr_point.coordinates[m], 2.0);
 			}
 
 			dist = sqrt(sum);
@@ -105,30 +102,46 @@ void findNearestCentroids(vector<Point> points, vector<Point> centroids) {
 	}
 }
 
-vector<Point> averageLabelCentroids(vector<Point> points, vector<Cluster> clusters) {
+vector<Centroid> averageLabeledCentroids(vector<Point> points, vector<Centroid> centroids) {
 	int total_points = points.size();
+	vector<Centroid> retval;
 
 	for (int i = 0; i < k; i++) {
 		for (int j = 0; j < total_points; j++) {
 			float sum = 0;
-			int cluster_points = clusters[i].points.size();
 
-			for (m = 0; m < total_points; m++) {
-				sum += clusters[i].points[m].getCoordinate(j);
+			for (int m = 0; m < total_points; m++) {
+				sum += centroids[i].points[m].coordinates[j];
 			}
-			clusters[i]
+			centroids[i].coordinates[j] = sum / total_points;
 		}
+		retval.push_back(centroids[i]);
+	}
+
+	return retval;
+}
+
+void kmeans(vector<Point> dataset, int k) {
+	vector<Centroid> centroids = randomCentroids(dataset, k);
+
+	int iters = 0;
+	vector<Centroid> old_centroids;
+
+	bool done = false;
+
+	while (!done) {
+		old_centroids = centroids;
+		iters++;
+
+		findNearestCentroids(dataset, centroids);
+		centroids = averageLabeledCentroids(dataset, centroids);
+
+		done = iters > iterations;
 	}
 }
 
-
 int main (int argc, char **argv) {
 	int c;
-	int k;
-	float threshold;
-	int iterations;
-	int workers;
-	char* input;
 	int option_index = 0;
 
 	static struct option long_options[] = {
