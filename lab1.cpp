d25e79c6a9a22b2982fa731058b37fc04f11b206
shreyastawre <tawre.shreyas@gmail.com>
2018-02-13 00:51:21 -0600

    Another commit

--- lab1.cpp
+++ lab1.cpp
@@ -147,7 +147,7 @@ vector<Centroid> averageLabeledCentroids(vector<Point> points, vector<Centroid> 
 				Point p = centroids[i].getPoint(m);
 				printf("%f + ", p.getCoordinate(j));
 				sum += p.getCoordinate(j);
-				printf("= %f\n", sum;);
+				printf("= %f\n", sum);
 			}
 			centroids[i].setCoordinate(j, sum / total_points);
 		}
