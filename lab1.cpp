0830d99a0813817ad0a1cee3e37e9aab6cbc8860
Shreyas Tawre <stawre@cs.utexas.edu>
2018-02-12 20:42:25 -0600

    Naive Solution

--- lab1.cpp
+++ lab1.cpp
@@ -4,6 +4,9 @@
 #include <getopt.h>
 #include <vector>
 #include <cmath>
+#include <sstream>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
@@ -14,9 +17,10 @@ int workers;
 char* input;
 
 class Point {
-public:
+private:
 	vector<float> coordinates;
-	int label;
+
+public:
 
 	Point(vector<float>& coordinates) {
 		int size = coordinates.size();
@@ -28,40 +32,60 @@ public:
 	float getDimensionsCount() {
 		return coordinates.size();
 	}
+
+	float getCoordinate(int index) {
+		return coordinates[index];
+	}
 };
 
 class Centroid {
-public:
-	int label;
+private:
 	vector<Point> points;
 	vector<float> coordinates;
 
-	Centroid(int label, Point point) {
-		this->label = label;
+public:
+	Centroid(Point point) {
 		int count = point.getDimensionsCount();
 		for(int i = 0; i < count; i++) {
-			coordinates.push_back(point.coordinates[i]);
+			coordinates.push_back(point.getCoordinate(i));
 		}
 		points.push_back(point);
+	}
+
+	Point getPoint(int index)
+	{
+		return points[index];
+	}
+
+	float getCoordinate(int index) {
+		return coordinates[index];
+	}
+
+	void setCoordinate(int index, float coordinate) {
+		coordinates[index] = coordinate;
+	}
+
+	int getSize() {
+		return points.size();
+	}
+
+	void addPoint(Point point)
+	{
+		points.push_back(point);
+	}
+
+	int getD() {
+		return coordinates.size();
 	}
 };
 
 vector<Centroid> randomCentroids(vector<Point> points, int k) {
 	vector<Centroid> retval;
 
-	for(int i = 0; i < k; i++) {
-		while (1) {
-			int index = rand() % points.size();
-			if (i > 0) {
-				if (retval[i-1].coordinates == points[i].coordinates) {
-					break;
-				}
-			}
-
-			Centroid* centroid = new Centroid(i, points[index]);
-			retval.push_back(*centroid);
-			break;
-		}
+	for (int i = 1; i < k + 1; i++) {
+		Centroid centroid(points[i]);
+		retval.push_back(centroid);
+		printf("%f\n", retval[i-1].getCoordinate(0));
 	}
 
 	return retval;
@@ -70,7 +94,7 @@ vector<Centroid> randomCentroids(vector<Point> points, int k) {
 
 void findNearestCentroids(vector<Point> points, vector<Centroid> centroids) {
 	int total_points = points.size();
-	int total_coordinates = points[0].getDimensionsCount();
+	int total_coordinates = points[1].getDimensionsCount();
 
 	for (int i = 0; i < total_points; i++) {
 		Point curr_point = points[i];
@@ -79,7 +103,7 @@ void findNearestCentroids(vector<Point> points, vector<Centroid> centroids) {
 		float min;
 
 		for (int j = 0; j < total_coordinates; j++) {
-			sum_diff += pow(centroids[0].coordinates[j] - curr_point.coordinates[j], 2.0);
+			sum_diff += pow(centroids[0].getCoordinate(j) - curr_point.getCoordinate(j), 2.0);
 		}
 
 		min = sqrt(sum_diff);
@@ -89,36 +113,52 @@ void findNearestCentroids(vector<Point> points, vector<Centroid> centroids) {
 			float sum = 0;
 
 			for(int m = 0; m < total_coordinates; m++) {
-				sum += pow(centroids[j].coordinates[m] - curr_point.coordinates[m], 2.0);
+				sum += pow(centroids[j].getCoordinate(m) - curr_point.getCoordinate(m), 2.0);
 			}
 
 			dist = sqrt(sum);
 
 			if(min > dist) {
 				min = dist;
-				curr_point.label = i;
+				centroids[j].addPoint(curr_point);
 			}
 		}
 	}
 }
 
 vector<Centroid> averageLabeledCentroids(vector<Point> points, vector<Centroid> centroids) {
-	int total_points = points.size();
+	int d = centroids[1].getD();
+	int total_points = centroids[1].getSize();
 	vector<Centroid> retval;
 
 	for (int i = 0; i < k; i++) {
-		for (int j = 0; j < total_points; j++) {
+		for (int j = 0; j < d; j++) {
 			float sum = 0;
-
 			for (int m = 0; m < total_points; m++) {
-				sum += centroids[i].points[m].coordinates[j];
+				Point p = centroids[i].getPoint(m);
+				sum += p.getCoordinate(j);
 			}
-			centroids[i].coordinates[j] = sum / total_points;
+			centroids[i].setCoordinate(j, sum / total_points);
 		}
 		retval.push_back(centroids[i]);
 	}
 
 	return retval;
+}
+
+bool converged(vector<Centroid> centroids, vector<Centroid> old_centroids) {
+	int total = centroids.size();
+	int d = centroids[0].getD();
+	int diff;
+	for (int i = 0; i < total; i++) {
+		for (int j = 0; j < d; j++) {
+			diff = abs(centroids[i].getCoordinate(j) - old_centroids[i].getCoordinate(j));
+			if (diff > threshold)
+				return false;
+		}
+	}
+
+	return true;
 }
 
 void kmeans(vector<Point> dataset, int k) {
@@ -133,10 +173,26 @@ void kmeans(vector<Point> dataset, int k) {
 		old_centroids = centroids;
 		iters++;
 
-		findNearestCentroids(dataset, centroids);
-		centroids = averageLabeledCentroids(dataset, centroids);
+		findNearestCentroids(dataset, centroids);		
+		centroids = averageLabeledCentroids(dataset, centroids);		
 
-		done = iters > iterations;
+		if (iterations > 0) {
+			done = iters > iterations || converged(centroids, old_centroids);
+		} else {
+			done = converged(centroids, old_centroids);
+		}
+	}
+
+	printf("Converged in %d iterations\n", iters);
+
+	int d = centroids[0].getD();
+
+	for (int i = 0; i < k; i++) {
+		printf("Cluster %d center: ", i);
+		for (int j = 0; j < d; j++) {
+			printf("%f ", centroids[i].getCoordinate(j));
+		}
+		printf("\n");
 	}
 }
 
@@ -181,4 +237,67 @@ int main (int argc, char **argv) {
 	}
 
 	printf("k: %d, threshold: %f, iterations: %d, workers: %d, input: %s\n", k, threshold, iterations, workers, input);
+
+	// Reading in input from a .txt file
+
+	int rows;
+	ifstream inFile;
+	string line;
+	string s;
+
+	inFile.open("input.txt");
+
+	inFile >> rows;
+
+	printf("Rows: %d\n", rows);
+
+	vector<Point> dataset;
+			vector<float> coordinates;
+					int d;
+
+
+	while (getline(inFile, line)) {
+		istringstream iss(line);
+		float x;
+		iss >> x;
+		d = 0;
+		while(iss >> s)
+        {
+        	d++;
+        	x = stof(s);
+        	printf("%f ", x);
+            coordinates.push_back(x);
+        }
+        printf("\n");
+	}
+	printf("D: %d\n", d);
+	Point point(coordinates);
+    dataset.push_back(point);
+
+    int i = d;
+    while (i < d*rows) {
+		vector<float> c;
+
+    	for (int j = i; j < (i + d); j++) {
+	    	c.push_back(dataset[0].getCoordinate(j));
+	    }
+
+	    Point point(c);
+	    dataset.push_back(point);
+
+	    i += 4;
+    }
+
+	Point x = dataset[0];
+	float y = x.getCoordinate(0);
+	printf("Here\n");
+
+	for (int a = 0; a < rows; a++) {
+			printf("%f %f %f %f\n", dataset[a].getCoordinate(0), 
+				dataset[a].getCoordinate(1), dataset[a].getCoordinate(2), 
+				dataset[a].getCoordinate(3));
+
+	}
+
+	kmeans(dataset, k);
 }
